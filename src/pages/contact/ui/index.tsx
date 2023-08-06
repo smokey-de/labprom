a7c4db442292949fd244edc0990a0780b9b92eac
Abrorxon <abrorxon158@gmail.com>
2023-08-06 22:34:09 +0500

    tg bot added

--- src/pages/contact/ui/index.tsx
+++ src/pages/contact/ui/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import style from './index.module.scss';
-import { Text, Title } from '@mantine/core';
-
+import styles from '@/widgets/layout/footer/index.module.scss';
+import { Box, Button, Text, Textarea, TextInput, Title } from '@mantine/core';
+import { useForm } from '@mantine/form';
+const bot = {
+  token: '6467635323:AAGtvPazdj4zkpFq96D9wqCQFUVa-iKe5gA',
+  chatId: '1452109307'
+}
 export const Contact = () => {
+  const form = useForm({
+    initialValues: {
+      email: '',
+      number: '',
+      message: '',
+    },
+
+    validate: {
+      email: (value: string) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      number: (value: string) => (/^\d+$/.test(value) ? null : 'Invalid number'),
+      message: (value: string) => (value.length > 10 ? null : 'Message is too short'),
+    },
+  });
   return (
     <>
       <div className={style.sectionWrapper}>
@@ -18,6 +36,63 @@ export const Contact = () => {
               This is a page with some basic contact information, such as an address and phone number. You might also
               try a plugin to add a contact form.
             </Text>
+            <div className={styles.footerCenter}>
+              <Box  maw={500} mx="auto" mt={20} style={{
+                rowGap: "20px",
+              }}>
+                <form onSubmit={form.onSubmit((values) => {
+                  fetch(`https://api.telegram.org/bot${bot.token}/sendMessage?chat_id=${bot.chatId}&text=${
+                    'Email: ' + values.email + '\n ' +
+                    'Number: ' + values.number + '\n ' +
+                    'Message: ' + values.message
+                  }`,{
+                    method: 'GET',
+                  }).then((res) => {
+                    if (res.status === 200){
+                      alert('Message sent successfully')
+                      form.reset()
+                    }else {
+                      alert('Error')
+                    }
+                  })
+                })}>
+                  <TextInput
+                    classNames={{
+                      required: styles.none
+                    }}
+                    className={styles.input}
+                    withAsterisk
+                    label="Email"
+                    placeholder="your@email.com"
+                    {...form.getInputProps('email')}
+                  />
+                  <TextInput
+                    classNames={{
+                      required: styles.none
+                    }}
+                    className={styles.input}
+                    withAsterisk
+                    label="Phone number"
+                    placeholder="Phone number"
+                    {...form.getInputProps('number')}
+                  />
+                  <Textarea
+                    classNames={{
+                      required: styles.none
+                    }}
+                    className={styles.input}
+                    placeholder="Your comment"
+                    label="Your comment"
+                    withAsterisk
+                    {...form.getInputProps('message')}
+                  />
+                  <Button type="submit" className={styles.formBtn}>
+                    Send
+                  </Button>
+                </form>
+              </Box>
+            </div>
+
           </div>
         </div>
       </div>
--- src/widgets/layout/footer/index.tsx
+++ src/widgets/layout/footer/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import style from './index.module.scss';
-import { Anchor, Button, Text, Title } from '@mantine/core';
+import { Anchor, Button, Text, Title, Box, TextInput, Textarea } from '@mantine/core';
 import { ReactComponent as IconOne } from '@/shared/images/icon-yelp.svg';
 import { ReactComponent as IconTwo } from '@/shared/images/icon-facebook.svg';
 import { ReactComponent as IconThree } from '@/shared/images/icon-twitter.svg';
@@ -8,35 +8,40 @@ import { ReactComponent as IconFour } from '@/shared/images/icon-instagram.svg';
 import { ReactComponent as IconFive } from '@/shared/images/icon-mail.svg';
 import { useMediaQuery, useWindowScroll } from '@mantine/hooks';
 
+
 const Footer: FC = () => {
+
   const [scroll, scrollTo] = useWindowScroll();
   const matches = useMediaQuery('(max-width: 780px)');
+
+
   return (
     <>
       <div className={style.footerWrapper}>
         <div className={style.footer}>
           <div className={style.footerTop}>
-            <Anchor>
-              <div className={style.icon}>
-                <IconOne />
-              </div>
+            <Anchor href='https://www.yelp.com/'>
+                <div className={style.icon}>
+                  <IconOne />
+                </div>
+
             </Anchor>
-            <Anchor>
+            <Anchor href={'https://www.facebook.com/WordPress/'}>
               <div className={style.icon}>
                 <IconTwo />
               </div>
             </Anchor>
-            <Anchor>
+            <Anchor href={'https://twitter.com/wordpress'}>
               <div className={style.icon}>
                 <IconThree />
               </div>
             </Anchor>
-            <Anchor>
+            <Anchor href={'https://www.instagram.com/explore/tags/wordcamp/'}>
               <div className={style.icon}>
                 <IconFour />
               </div>
             </Anchor>
-            <Anchor>
+            <Anchor >
               <div className={style.icon}>
                 <IconFive />
               </div>
